fa5d4eeb70b8df9256be5375cc1203406d6da414
zehao Gao <126786309+772736243@users.noreply.github.com>
2023-11-30 18:29:19 +0800

    Add files via upload

---
+++ yolov8s-wiou.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+# model = YOLO('yolov8s.pt')
+# results = model.train(data='data.yaml', epochs=300)
+
+# model = YOLO("D:\PytorchPro\\ultralytics\\runs\detect\\train14\weights\last.pt")
+# results = model.val(data='data.yaml')
+
+
+
